2cd6c274794baa0a338970517a07f618af8c2cf0
sonuJha108 <codinghub650@gmail.com>
2025-01-04 12:33:54 +0530

    add the feature page and modif the home page

---
+++ lib/pages/feature_lists.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:voice_app/colors/pallete.dart';
+
+class FeatureBox extends StatelessWidget {
+  // init the color in the feature box
+  final Color color;
+
+  // write the defualt const. and make sure the current instence are enter
+  final String headerText;
+
+  // description text
+  final String descriptionText;
+
+  const FeatureBox({
+    super.key,
+    required this.color,
+    required this.headerText,
+    required this.descriptionText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 35,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0).copyWith(left: 15),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                headerText,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Text(
+                descriptionText,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
